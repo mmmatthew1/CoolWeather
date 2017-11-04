3c0c2ba205520d7b89785805f9ba93861ba37cf5
Matthew <swd-004@mail.foxconn.com>
2017-11-04 10:09:37 +0800

    Add database config

---
+++ app/src/main/java/com/foxconn/matthew/coolweather/db/Conty.java
@@ -0,0 +1,50 @@
+package com.foxconn.matthew.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Matthew on 2017/11/4.
+ */
+
+public class Conty extends DataSupport {
+
+    private int id;
+
+    private String contyName;
+
+    private String weatherId;
+
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContyName() {
+        return contyName;
+    }
+
+    public void setContyName(String contyName) {
+        this.contyName = contyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
---
+++ app/src/main/java/com/foxconn/matthew/coolweather/db/Province.java
@@ -0,0 +1,40 @@
+package com.foxconn.matthew.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Matthew on 2017/11/4.
+ */
+
+public class Province extends DataSupport {
+
+    private int id;
+
+    private String provinceName;
+
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
